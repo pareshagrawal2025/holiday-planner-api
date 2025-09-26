c8576d6c0be5f5c8242ebd144b5dbdd08f7ebeb5
pareshagrawal2025 <pareshagrawal@gmail.com>
2025-09-26 07:41:30 +0200

    new enhancements-2.1.0

--- src/main/java/com/example/holidayplanner/exception/InvalidParameterException.java
+++
@@ -1,8 +0,0 @@
-package com.example.holidayplanner.exception;
-
-// Custom exception class to handle invalid parameters in API requests
-public class InvalidParameterException extends RuntimeException {
-    public InvalidParameterException(String message) {
-        super(message);
-    }
-}
